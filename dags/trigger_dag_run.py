7938e9c47804ef11d9b94e91c8bd731ddf3e4f0f
bikash-jha2829 <79247013+bikash-jha2829@users.noreply.github.com>
2023-03-13 19:26:49 +0100

    Update trigger_dag_run.py

--- dags/trigger_dag_run.py
+++ dags/trigger_dag_run.py
@@ -3,28 +3,23 @@ from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from datetime import datetime
 from airflow.operators.empty import EmptyOperator
 
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1
-}
-
-dag = DAG('master_usage_dagrun', default_args=default_args, schedule_interval='@once')
-
-start_task = EmptyOperator(task_id='start_usage_stack')
-end_task = EmptyOperator(task_id='end_usage_task')
+with DAG(
+        dag_id="master_usage_dagrun",
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        schedule=None,
+        tags=["RunDataflowJob"],
+) as dag:
+    start_task = EmptyOperator(task_id='start_usage_stack')
+    end_task = EmptyOperator(task_id='end_usage_task')
 
 # Define the TriggerDagRunOperator
-trigger_dag_op = TriggerDagRunOperator(
-    task_id='trigger_child_aum_job',
-    trigger_dag_id='target_dag_id',
-    dag=dag,
-)
+    trigger_dag_op = TriggerDagRunOperator(
+        task_id='trigger_child_aum_job',
+        trigger_dag_id='target_dag_id',
+        dag=dag,
+    )
 
-start_task >> trigger_dag_op >> end_task
+    start_task >> trigger_dag_op >> end_task
 
 # target_dag_id is the ID of the DAG you want to trigger
